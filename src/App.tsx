cd93f227aa51f77a058350e83adbde29e3f657cc
Babir Ali <Babir.Ali@corp.aaj.com>
2019-01-16 21:27:12 +0500

    added project

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'font-awesome/css/font-awesome.css'
+import jQuery from 'jquery'
+import 'bootstrap'
+import Popper from 'popper.js';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <p>test</p>
+      </div>
+    );
+  }
+}
+
+export default App;
